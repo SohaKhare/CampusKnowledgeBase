d46f230bc3e37fc4fb263a1092e62768eb0100b4
SohaKhare <sohaajaykhare2006@gmail.com>
2025-12-25 04:45:23 +0530

    token expiry

--- aiml/config.py
+++ aiml/config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 class Config:
     GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
@@ -6,3 +7,4 @@ class Config:
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     FRONTEND_URL = os.getenv("FRONTEND_URL")
     STUDENT_EMAIL_DOMAIN = os.getenv("STUDENT_EMAIL_DOMAIN")
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=3)
