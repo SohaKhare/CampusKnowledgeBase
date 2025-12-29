0066364045f00598c2289ea7cad5e4ad779e11cb
GamingWithShaurya <shauryakesarwani19@gmail.com>
2025-12-29 21:22:43 +0530

    flask fitrx

--- aiml/main.py
+++ aiml/main.py
@@ -114,4 +114,4 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000, use_reloader=False)
+    app.run(host="0.0.0.0", debug=True, port=8000, use_reloader=False)
